b4ab460b26d85b695f380cb9c0f3d2b476b05963
Loboxos <vcristian528@gmail.com>
2023-05-14 20:29:09 -0300

    "creacion de modelos 'Producto ' y 'proveedor' "

--- compra/models.py
+++ compra/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+class Proveedor(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    dni=models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.nombre,self.apellido,self.dni
 
+class Producto(models.Model):
+    nombre=models.CharField(max_length=25)
+    precio=models.FloatField(default=0)
+    stock_actual=models.ImageField(default=0)
+    proveedor=models.ForeignKey(Proveedor,related_name="proveedor",on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.nombre}\n{self.precio}\n{self.stock_actual}\n{self.proveedor}"
 # Create your models here.
