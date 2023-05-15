847d1e6b799489d4521867d35e906b26f15d7a9e
Loboxos <vcristian528@gmail.com>
2023-05-15 12:20:17 -0300

    view agregar producto y agregar proveedor creadas

--- compra/urls.py
+++ compra/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from.views import listado_productos
+from.views import listado_productos,agregar_producto
 
 urlpatterns=[
-    path("listado_productos/",listado_productos,name="listado_productos")
-  
+    path("listado_productos/",listado_productos,name="listado_productos"),
+    path("productos/nuevo",agregar_producto,name="agregar_producto")
 ]
--- compra/views.py
+++ compra/views.py
@@ -7,4 +7,49 @@ def listado_productos(request):
         "listado_productos":lista_productos
     }
     return render(request,"compra/listado_productos.html",context)
+
+def agregar_producto(request):
+    lista_proveedores = Proveedor.objects.all()
+
+    context = {
+        "listado_proveedores": lista_proveedores
+    }
+
+    if request.POST:
+        nombre_producto = request.POST["nombre"]
+        precio_producto = request.POST["precio"]
+        stock_actualP=request.POST["stock_actual"]
+        proveedor_id = request.POST["proveedor"]
+
+    
+        Producto.objects.create(
+            nombre=nombre_producto,
+            precio=precio_producto,
+            stock_actual=stock_actualP,
+            proveedor=proveedor_id
+        )
+
+    return render(
+        request,
+        "compra/agregar_producto.html",
+        context
+    )
+def agregar_proveedor(request):
+
+    if request.POST:
+        nombre_proveedor = request.POST["nombre"]
+        apellido_proveedor = request.POST["precio"]
+        dni_proveedor = request.POST["dni"]
+
+        Producto.objects.create(
+            nombre=nombre_proveedor,
+            apellido=apellido_proveedor,
+            dni=dni_proveedor
+        )
+
+    return render(
+        request,
+        "compra/agregar_proveedor.html"
+    )
+
 # Create your views here.
